032d32bdf1ce2b24f6ab9e1f9b4b5ec7e99d5356
Nguyễn Văn Hậu <haunguyenen48@gmail.com>
2026-08-04 19:55:45 +0700

    Refine marketing promotion rules and pricing UI

--- WebBanHang/Models/ViewModel/MarketingSellingVM.cs
+++ WebBanHang/Models/ViewModel/MarketingSellingVM.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebBanHang.Models.ViewModel
 {
@@ -26,12 +27,18 @@ namespace WebBanHang.Models.ViewModel
         public int ProductID_B { get; set; }
         public string ProductNameA { get; set; }
         public string ProductNameB { get; set; }
+        public string ProductImageA { get; set; }
         public string ProductImageB { get; set; }
+        public decimal ProductPriceA { get; set; }
+        public decimal ProductPriceB { get; set; }
         public string Code { get; set; }
         public decimal DiscountAmount { get; set; }
+        public decimal OriginalAmount => ProductPriceA + ProductPriceB;
+        public decimal DiscountedAmount => Math.Max(0m, OriginalAmount - DiscountAmount);
         public int Support { get; set; }
         public decimal Confidence { get; set; }
         public decimal ActualUtility { get; set; }
+        public decimal MinimumMarginPct { get; set; }
         public DateTime EndDate { get; set; }
     }
 
@@ -41,8 +48,28 @@ namespace WebBanHang.Models.ViewModel
         public int ActiveComboOffers { get; set; }
         public int RedeemedVouchers { get; set; }
         public decimal RevenueInfluenced { get; set; }
+        public MarketingSettingsVM Settings { get; set; } = new MarketingSettingsVM();
         public List<PersonalVoucherVM> RecentPersonalVouchers { get; set; } = new List<PersonalVoucherVM>();
         public List<ComboOfferVM> ActiveCombos { get; set; } = new List<ComboOfferVM>();
+    }
+
+    public class MarketingSettingsVM
+    {
+        [Range(0.01, 100)] public decimal PersonalDiscountPct { get; set; } = 5m;
+        [Range(1000, 100000000)] public decimal PersonalMaxDiscountAmount { get; set; } = 150000m;
+        [Range(0, 100000)] public decimal PersonalMinInterestScore { get; set; } = 8m;
+        [Range(1, 720)] public int VoucherValidityHours { get; set; } = 72;
+        [Range(0, 365)] public int VoucherCooldownDays { get; set; } = 7;
+        [Range(0, 100)] public decimal PersonalMinimumMarginPct { get; set; } = 5m;
+
+        [Range(0.01, 100)] public decimal ComboDiscountPct { get; set; } = 5m;
+        [Range(1000, 100000000)] public decimal ComboMaxDiscountAmount { get; set; } = 300000m;
+        [Range(1, int.MaxValue)] public int ComboMinSupport { get; set; } = 2;
+        [Range(0, 1)] public decimal ComboMinConfidence { get; set; } = 0.20m;
+        [Range(0, 1000000000)] public decimal ComboMinUtility { get; set; } = 100000m;
+        [Range(1, 365)] public int ComboValidityDays { get; set; } = 14;
+        [Range(1, 1000000)] public int ComboUsageLimit { get; set; } = 100;
+        [Range(0, 100)] public decimal ComboMinimumMarginPct { get; set; } = 5m;
     }
 
     public class PromotionEvaluation
@@ -55,5 +82,11 @@ namespace WebBanHang.Models.ViewModel
         public int? CouponID { get; set; }
         public int? CustomerCouponID { get; set; }
         public int? ComboOfferID { get; set; }
+        public int? ProductID_A { get; set; }
+        public int? ProductID_B { get; set; }
+        public string ProductNameA { get; set; }
+        public string ProductNameB { get; set; }
+        public decimal OriginalAmount { get; set; }
+        public decimal FinalAmount => Math.Max(0m, OriginalAmount - DiscountAmount);
     }
 }
